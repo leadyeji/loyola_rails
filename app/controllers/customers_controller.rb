2f9bd7354368a30907cf427d5bd12eba27f6ee95
robert <rboling91@gmail.com>
2014-09-14 01:31:28 -0400

    create customers from post requests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,46 @@
 class CustomersController < ApplicationController
+  before_action :set_merchant, only: [:show, :edit, :update, :destroy]
+# GET /customers
+  # GET /customers.json
+  def index
+    @customers = Customer.all
+  end
+
+  # GET /customers/1
+  # GET /customers/1.json
+  def show
+  end
+
+  # GET /customers/new
+  def new
+    @customer = Customer.new
+  end
+
+  # GET /customers/1/edit
+  def edit
+  end
+
+  # POST /customers
+  # POST /customers.json
+  def create
+    @customer = Customer.new(test_params)
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to @customer, notice: 'Test was successfully created.' }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_customer
+      @merchant = Merchant.find(params[:id])
+    end
+    def merchant_params
+      params.require(:customer).permit(:name, :phone_number)
+    end
 end
